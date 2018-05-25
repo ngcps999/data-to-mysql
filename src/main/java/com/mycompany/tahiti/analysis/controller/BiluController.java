5cba8600ea1f33e9d2d3e90e05efdb2279a9b25a
xiaodingxiaoke <xiaodingxiaoke@163.com>
2018-05-25 18:04:02 +0800

    add biluController

--- src/main/java/com/mycompany/tahiti/analysis/controller/BiluController.java
+++ src/main/java/com/mycompany/tahiti/analysis/controller/BiluController.java
@@ -1,31 +1,62 @@
 package com.mycompany.tahiti.analysis.controller;
 
+import com.mycompany.tahiti.analysis.configuration.Configs;
+import com.mycompany.tahiti.analysis.jena.TdbJenaLibrary;
 import com.mycompany.tahiti.analysis.model.BiluBaseInfo;
 import com.mycompany.tahiti.analysis.model.BiluRichInfo;
 import com.mycompany.tahiti.analysis.model.Person;
 import io.swagger.annotations.Api;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.Statement;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/bilus")
 @Api(description = "Bilu controller")
 public class BiluController {
+    @Autowired
+    TdbJenaLibrary tdbJenaLibrary;
+
     @ResponseBody
     @GetMapping("/{biluId}")
     public BiluRichInfo getBiluById(@PathVariable("biluId") String biluId) {
-        BiluRichInfo bilu = new BiluRichInfo();
-        bilu.setId("ssdsdsdfdffd");
-        bilu.setName("王大锤的笔录");
-        bilu.setContent("我叫王大锤，我一开始只想着\n" +
-                "不用多久 我就会升职加薪 当上总经理 出任CEO 迎娶白富美 走向人生巅峰 想想还有点小激动呢\n" +
-                "万万没想到, 她说这人好像条狗啊\n" +
-            "他说得好有道理 我竟无言以对");
-        List<String> tags = Arrays.asList(new String[]{"狗", "CEO"});
-        bilu.setTags(tags);
+        try{
+            tdbJenaLibrary.openReadTransaction();
+            Model model = tdbJenaLibrary.getModel(Configs.getConfig("jenaModelName"));
+            Iterator<Statement> statements_iterator = tdbJenaLibrary.getStatementsById(model,biluId);
+            Resource bilu_subject = null;
+            while(statements_iterator.hasNext()){
+                bilu_subject = statements_iterator.next().getSubject();
+                break;
+            }
+            if(bilu_subject==null)return null;
 
-        return bilu;
+            //bilu name
+            List<String> bilu_name_list = tdbJenaLibrary.getStringValueBySP(model,bilu_subject,"common:type.object.name");
+            String bilu_name = bilu_name_list.size()>0?bilu_name_list.get(0):"";
+            //bilu content
+            List<String> bilu_content_list = tdbJenaLibrary.getStringValueBySP(model,bilu_subject,"common:common.document.contentStreame");
+            String bilu_content = bilu_content_list.size()>0?bilu_content_list.get(0):"";
+            //bilu tags
+            List<String> tags = tdbJenaLibrary.getStringValueBySP(model,bilu_subject,"common:type.object.tag");
+
+            BiluRichInfo bilu = new BiluRichInfo();
+            bilu.setId(biluId);
+            bilu.setName(bilu_name);
+            bilu.setContent(bilu_content);
+            bilu.setTags(tags);
+
+            return bilu;
+
+        }finally {
+            tdbJenaLibrary.closeTransaction();
+        }
     }
 }
