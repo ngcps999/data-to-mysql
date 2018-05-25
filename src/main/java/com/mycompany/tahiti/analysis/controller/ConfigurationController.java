97d29e0806ddab0c723ddd162125f9a778984b34
xinge <xingealpha@gmail.com>
2018-05-25 14:58:45 +0800

    Update code about re-create tdb jena library

--- src/main/java/com/mycompany/tahiti/analysis/controller/ConfigurationController.java
+++ src/main/java/com/mycompany/tahiti/analysis/controller/ConfigurationController.java
@@ -1,7 +1,9 @@
 package com.mycompany.tahiti.analysis.controller;
 
 import com.mycompany.tahiti.analysis.configuration.Configs;
+import com.mycompany.tahiti.analysis.jena.TdbJenaLibrary;
 import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -9,8 +11,11 @@ import org.springframework.web.bind.annotation.*;
 @Api(description = "configuration controller")
 public class ConfigurationController {
 
+    @Autowired
+    TdbJenaLibrary tdbJenaLibrary;
     @GetMapping("/updateJenaModelName/{jenaModelName}")
     public void updateJenaModelName(@PathVariable("jenaModelName") String jenaModelName) {
         Configs.addConfig("jenaModelName", jenaModelName);
+        tdbJenaLibrary = new TdbJenaLibrary(Configs.getConfig("tdbName"));
     }
 }
