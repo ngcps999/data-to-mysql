32ccef0152fce7a5a5b85fc1ed79895aab3fdc0a
qxwang <syn6648@dingtalk.com>
2018-06-27 20:19:36 +0800

    get case basic info

---
+++ src/main/java/com/mycompany/tahiti/analysis/repository/CaseMongo.java
@@ -0,0 +1,28 @@
+package com.mycompany.tahiti.analysis.repository;
+
+import lombok.Data;
+
+@Data
+public class CaseMongo {
+    private String ZBR_XM;
+    private String CBDW_BH;
+    private String AJLX;
+    private String ZBR_SFZH;
+    private String XYR_XM;
+    private String AJMC;
+    private String CBDW_MC;
+    private String CBQY_BH;
+    private String FADD;
+    private String AJZTName;
+    private String LASJ;
+    private String JQBH;
+    private String AJBH;
+    private String AJLXName;
+    private String LRSJ;
+    private String ID;
+    private String JYAQ;
+    private String SLSJ;
+    private String AJZT;
+    private String SLDW_MC;
+    private String update_time;
+}
